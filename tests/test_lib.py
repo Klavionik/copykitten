e0753c014ffbec72bbd41f5307bb4d8b77bb92a4
Roman Vlasenko <klavionik@gmail.com>
2024-01-27 00:39:42 +0300

    Cover paste and clear with tests

--- tests/test_lib.py
+++ tests/test_lib.py
@@ -1,22 +1,68 @@
+import os
 import subprocess
 from time import sleep
 from unittest import TestCase
 
 import copykitten
 
+DEFAULT_ITERATIONS = 300
+DEFAULT_SLEEP_TIME = 0.1
+
+try:
+    ITERATIONS = int(os.getenv("COPYKITTEN_TEST_ITERATIONS", DEFAULT_ITERATIONS))
+except Exception:
+    ITERATIONS = DEFAULT_ITERATIONS
+
+try:
+    SLEEP_TIME = int(os.getenv("COPYKITTEN_TEST_SLEEP_TIME", DEFAULT_SLEEP_TIME))
+except Exception:
+    SLEEP_TIME = DEFAULT_SLEEP_TIME
+
 
 def read_clipboard() -> str:
     return subprocess.check_output(["xsel", "-b"]).decode()
 
 
-class TestCopy(TestCase):
+def write_clipboard(content: str) -> None:
+    subprocess.check_output(["xsel", "-b"], encoding="utf-8", input=content)
+
+
+class TestClipboard(TestCase):
+    # Why sleep in tests?
+    # It may take a bit for the OS to finish the clipboard operation,
+    # so there have to be a short delay before asserting the result.
+    # Otherwise, tests may randomly fail.
     def test_copy(self):
-        for i in range(300):
+        for i in range(ITERATIONS):
             text = f"text{i}"
 
             with self.subTest(text=text):
                 copykitten.copy(text)
-                sleep(0.1)
+                sleep(SLEEP_TIME)
                 actual = read_clipboard()
 
                 self.assertEqual(actual, text)
+
+    def test_paste(self):
+        for i in range(ITERATIONS):
+            text = f"text{i}"
+
+            with self.subTest(text=text):
+                write_clipboard(text)
+                sleep(SLEEP_TIME)
+                actual = copykitten.paste()
+
+                self.assertEqual(actual, text)
+
+    def test_clear(self):
+        for i in range(ITERATIONS):
+            text = f"text{i}"
+
+            with self.subTest(text=text):
+                write_clipboard(text)
+                sleep(SLEEP_TIME)
+                copykitten.clear()
+                sleep(SLEEP_TIME)
+                actual = read_clipboard()
+
+                self.assertEqual(actual, "")
